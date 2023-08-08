ad19c64fce69806ee3230d886d16107ba95bba1d
Barış Güngör <65550845+barisgungor10@users.noreply.github.com>
2023-08-08 10:14:33 +0300

    Create Band Name Generator.py

---
+++ Band Name Generator.py
@@ -0,0 +1,10 @@
+#1. Creating a greeting for your program.
+print("Welcome to the game")
+#2. Asking the user for the city that they grew up in.
+city = input("Which city you grew up in?: ")
+#3. Asking the user for the name of a pet.
+pet = input("What is your cat's name?: ")
+#4. Combining the name of their city and pet and show them their band name.
+brand_name = city + " " + pet
+#5. Making sure the input cursor shows on a new line:
+print(f"Your Brand Name is: {brand_name}")

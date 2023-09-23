2c1b685d6c797901615a366b3f6ed8f4da441c89
Barış Güngör <65550845+barisgungor10@users.noreply.github.com>
2023-09-23 14:10:41 +0300

    Update main.py

--- higher-or-lower/main.py
+++ higher-or-lower/main.py
@@ -5,31 +5,30 @@ from random import randint
 from replit import clear
 
 # Initialize game variables
-game_on = True
-score = 0
+game_on = True  # Flag to control the game loop
+score = 0  # Initialize the player's score
+
+# Choose random indices for two data items (celebrities)
+number_1 = randint(0, len(data) - 1)
+number_2 = randint(0, len(data) - 1)
 
 # Main game loop
 while game_on:
     # Display game logo
-
     print(logo)
 
     # Display current score if the score is greater than 0
     if score > 0:
         print(f"You're right! Current score: {score}\n")
 
-    # Choose random indices for two data items (celebrities)
-    number_1 = randint(0, len(data) - 1)
-    number_2 = randint(0, len(data) - 1)
-
-    # Ensure that number_1 and number_2 are different
-    if number_1 == number_2:
-        number_2 = (number_1 + 1) % len(data)
-
     # Display the two data items for comparison
-    print(f"Compare A: {data[number_1]['name']}, a {data[number_1]['description']}, from {data[number_1]['country']}\n")
+    print(
+        f"Compare A: {data[number_1]['name']}, a {data[number_1]['description']}, from {data[number_1]['country']}\n"
+    )
     print(vs)
-    print(f"Compare B: {data[number_2]['name']}, a {data[number_2]['description']}, from {data[number_2]['country']}\n")
+    print(
+        f"Compare B: {data[number_2]['name']}, a {data[number_2]['description']}, from {data[number_2]['country']}\n"
+    )
 
     # Prompt the user for their choice
     choice = input("Who has more followers? Type 'A' or 'B': ").lower()
@@ -39,8 +38,17 @@ while game_on:
 
     # Compare the follower counts and update the score
     if choice == 'a' and data[number_1]['follower_count'] >= data[number_2]['follower_count']:
+        # Remove the data item that was not chosen for the next round
+        data.pop(number_2)
+        # Generate a new random index for the remaining data
+        number_2 = randint(0, len(data) - 1)
         score += 1
     elif choice == 'b' and data[number_2]['follower_count'] >= data[number_1]['follower_count']:
+        # Remove the data item that was not chosen for the next round
+        data.pop(number_1)
+        # Update the indices for the remaining data
+        number_1 = number_2
+        number_2 = randint(0, len(data) - 1)
         score += 1
     else:
         # End the game if the user's choice is wrong
